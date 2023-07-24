053dab8498d035e593455d99709964d8910d2dd2
Yurii Tor <Yurii.tor@bofh.od.ua>
2023-07-24 21:00:15 +0300

    firebase

--- netlify/functions/capture-ip.js
+++ netlify/functions/capture-ip.js
@@ -1,4 +1,5 @@
 // netlify/functions/capture-ip.js
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   // Get the IP address from the headers
@@ -13,8 +14,36 @@ exports.handler = async function(event, context) {
     };
   }
 
-  // TODO: Handle storing the IP address and associated URL in your chosen database
-  console.log(`IP: ${ip}, URL: ${url}`);
+  // Generate the current date and time in the format yymmdd-hhmm
+  const now = new Date();
+  const datetime = now.getFullYear().toString().slice(-2) +
+    (now.getMonth() + 1).toString().padStart(2, '0') +
+    now.getDate().toString().padStart(2, '0') + '-' +
+    now.getHours().toString().padStart(2, '0') +
+    now.getMinutes().toString().padStart(2, '0');
+
+  // Construct the data to be stored
+  const data = {};
+  data[datetime] = { ip };
+
+  // Send a PUT request to your Firebase database
+  const firebaseUrl = 'https://your-firebase-database.firebaseio.com/path/to/your/data.json';
+  const response = await fetch(firebaseUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  // Check for errors
+  if (!response.ok) {
+    console.error('Failed to store IP address:', response.statusText);
+    return {
+      statusCode: 500,
+      body: 'Failed to store IP address',
+    };
+  }
 
   // Redirect the user
   return {
