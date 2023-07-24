5040ccbe30da8cd76b35a5686e470b362ec3abbd
Yurii Tor <Yurii.tor@bofh.od.ua>
2023-07-24 21:13:49 +0300

    post instead put

--- netlify/functions/capture-ip.js
+++ netlify/functions/capture-ip.js
@@ -29,7 +29,7 @@ exports.handler = async function(event, context) {
   // Send a PUT request to your Firebase database
   const firebaseUrl = 'https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink.json';
   const response = await fetch(firebaseUrl, {
-    method: 'PUT',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
