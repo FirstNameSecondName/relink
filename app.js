d3bb0616db30c1d4b61de573a3dc345dbed597af
Yurii Tor <Yurii.tor@bofh.od.ua>
2023-08-05 03:00:10 +0300

    datetime fix

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ app.get('*', async (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const userAgent = req.headers['user-agent'];
   const language = req.headers['accept-language'];
+  let dateTime = new Date().toISOString().replace(/[^0-9]/g, "").slice(2,12);
 
   // Відправка даних до Firebase
-  const response = await fetch('https://<your-firebase-db-url>.firebaseio.com/' + dateTime + '.json', {
+  const response = await fetch('https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink/' + dateTime + '.json', {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ ip: ip, userAgent: userAgent, language: language }),
