3d3e6789bd38fb58f6bf8cb9477332d651b318aa
Yurii Tor <Yurii.tor@bofh.od.ua>
2023-08-05 21:25:19 +0300

    capture functions

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-// Імпорт бібліотеки Express
 const express = require('express');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
+
 const app = express();
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
-// Створення маршруту GET, який обробляє всі вхідні запити
-app.get('*', async (req, res) => {
-  // Отримання IP-адреси та інших заголовків з запиту
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const userAgent = req.headers['user-agent'];
-  const language = req.headers['accept-language'];
-  const { timeZone, referrer } = req.body;
-  let dateTime = new Date().toISOString().replace(/[^0-9]/g, "").slice(2,12);
 
-  const expressRes = res;
-  // Відправка даних до Firebase
-  const response = await fetch('https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink/' + dateTime + '.json', {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ip: ip, userAgent: userAgent, language: language,
-		timeZone: timeZone,
-        referrer: referrer }),
-  }).then(firebaseRes  => {
-    console.log(req.query.url); // add this line to debug
-    expressRes.redirect(req.query.url);
-  })
-  .catch(err => {
-    expressRes.send("Error: " + err);
-  });
+// Маршрут для збору часового поясу і реферера
+app.post('/capture-info', async (req, res) => {
+    const { timeZone, referrer } = req.body;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+    const language = req.headers['accept-language'];
+    let dateTime = new Date().toISOString().replace(/[^0-9]/g, "").slice(2,12);
 
-  // Перенаправлення користувача до запитаного URL
-  //const requestedUrl = req.originalUrl.slice(1);
-  //res.redirect(requestedUrl);
+    // Відправка даних до Firebase
+    const response = await fetch('https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink/' + dateTime + '.json', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            ip: ip,
+            userAgent: userAgent,
+            language: language,
+            timeZone: timeZone,
+            referrer: referrer
+        }),
+    }).catch(err => {
+        res.status(500).send("Error: " + err);
+    });
+
+    res.status(200).send('Info captured');
 });
 
-// Запуск додатку на порті 3000
+// GET маршрут для редіректу
+app.get('/relink', (req, res) => {
+    if (req.query.url) {
+        res.sendFile(__dirname + '/public/index.html');
+    } else {
+        res.send('URL parameter is missing.');
+    }
+});
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
